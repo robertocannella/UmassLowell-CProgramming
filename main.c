81a24c4b61c8bf1c548463903898dfe501fb1ae9
robertocannella <robertocannella@gmail.com>
2023-01-18 21:09:36 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*--------------------------------------------------------------*/
+/*                                                              */
+/* HOMEWORK: fill out assignment number                         */
+/*                                                              */
+/* Name: Roberto Cannella                                       */
+/*                                                              */
+/* Class: C Programming                                         */
+/*                                                              */
+/* Date: 2023-01-31                                             */
+/*                                                              */
+/* Description: A brief description of the program.              */
+/*                                                              */
+/*--------------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
