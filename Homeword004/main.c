5f9b33e15b9b7a371d55ea9913b490925952ab13
robertocannella <robertocannella@gmail.com>
2023-02-13 21:25:45 -0500

    After Refactor, Add Average

--- Homeword004/main.c
+++ Homeword004/main.c
@@ -16,33 +16,25 @@
 
 #include <stdio.h>
 
-// constants to use
-#define SIZE 5           // number of employees to process
-#define STD_HOURS 40.0   // normal work week hours before overtime
-#define OT_RATE 1.5      // time and half overtime setting
+#define SIZE        5           // number of employees to process
+#define STD_HOURS   40.0        // normal work week hours before overtime
+#define OT_RATE     1.5         // time and half overtime setting
 
 int main()
 {
 
-
-    // Declare variables needed for the program
-    // Recommend an array for clock, wage, hours,
-    // ... and overtime hours and gross.
-    // Recommend arrays also for normal pay and overtime pay                
-    // It is OK to pre-fill clock and wage values ... or you can prompt for them
-
     // unique employee identifier
-    long int clock [SIZE] = {98401, 526488, 765349, 34645, 127615};
- 
-    float   hours [SIZE];        // hours worked in a given week
-    float   grossPay [SIZE];     // weekly gross pay - normal pay + overtime pay
-    float   normalPay [SIZE];    // normal weekly pay without any overtime
-    float   overtimeHours[SIZE]; // overtime hours worked in a given week
-    float   overtimePay [SIZE];  // overtime pay for a given week
-    float   totals [SIZE] = {};  // totals of each employee - initialize to zeros
+    long int    clock [SIZE] = {98401, 526488, 765349, 34645, 127615};
+
+    float       hours [SIZE];               // hours worked in a given week
+    float       grossPay [SIZE];            // weekly gross pay - normal pay + overtime pay
+    float       normalPay [SIZE];           // normal weekly pay without any overtime
+    float       overtimeHours[SIZE] = {};   // overtime hours worked in a given week
+    float       overtimePay [SIZE]  = {};   // overtime pay for a given week
+    float       totals [SIZE]       = {};   // totals of each column
 
     // hourly pay for each employee
-    float wage [SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35}; 
+    float       wage [SIZE] = {10.6, 9.75, 10.5, 12.25, 8.35};
 
     printf ("\n*** Pay Calculator ***\n\n");
 
@@ -51,40 +43,36 @@ int main()
     {
 
         // Prompt and Read in hours worked for employee
-        printf("\nEnter the number of hours the employee worked for %06li: ", clock[count]);
+        printf("\nEnter the number of hours for employee #%06li: ", clock[count]);
         scanf("%f", &hours[count]);     // Store into hours[]
 
-
         // Calculate overtime and gross pay for employee
-        if (hours[count] >= STD_HOURS)
-        {
-            overtimeHours[count] = hours[count] - STD_HOURS;
-            // TODO: Calculate arrays normalPay and overtimePay with overtime
-            normalPay[count] = STD_HOURS * wage[count];
-            overtimePay[count] = OT_RATE * overtimeHours[count] * wage[count];
+        if (hours[count] >= STD_HOURS){
+            overtimeHours[count]    = hours[count] - STD_HOURS;
+            // Calculate arrays normalPay and overtimePay with overtime
+            normalPay[count]        = STD_HOURS * wage[count];
+            overtimePay[count]      = OT_RATE * overtimeHours[count] * wage[count];
         }
-        else // no OT
-        {
-            overtimeHours[count] = 0;
-            // TODO: Calculate arrays normalPay and overtimePay without overtime
-            normalPay[count] = hours[count] * wage[count];
-            overtimePay[count] = 0;
+        else {
+            // Calculate arrays normalPay and overtimePay without overtime
+            normalPay[count]        = hours[count] * wage[count];
         }
 
         // Calculate Gross Pay
         grossPay[count] = normalPay[count] + overtimePay[count];
 
-
-        // Sum up totals
+        // Tally up columns
         totals[1] += wage[count];
         totals[2] += hours[count];
         totals[3] += overtimeHours[count];
         totals[4] += grossPay[count];
     }
 
-    // TODO: Print a nice table header
+    // Print a nice table header
+    printf("\n\n");
+    printf("\t                 *** Pay Calculator Report ***                    \n");
     printf("\t------------------------------------------------------------------\n");
-    printf("\t    Clock #        Wage        Hours           OT        Gross    \n");
+    printf("\t    Clock #        Wage     Tot Hours     OT Hours    Gross Pay    \n");
     printf("\t------------------------------------------------------------------\n");
 
     // Now that we have all the information in our arrays, we can
@@ -98,6 +86,10 @@ int main()
     // Print Totals
     printf("\t------------------------------------------------------------------\n");
     printf("\t    Totals %12.2f %12.1f %12.1f %12.2f   \n",totals[1],totals[2],totals[3],totals[4]);
+    printf("\t------------------------------------------------------------------\n");
+
+    // Print Averages
+    printf("\t  Averages %12.2f %12.1f %12.1f %12.2f   \n",totals[1]/SIZE,totals[2]/SIZE,totals[3]/SIZE,totals[4]/SIZE);
     printf("\t------------------------------------------------------------------\n");
     return(0);
 }
